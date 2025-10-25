d49dfe318e6cc6b7a9854ec23ce1b4b25b3e589c
vanessanasimba <nmvanessa25@gmail.com>
2025-10-24 21:03:08 -0500

    v - cambio de main

--- main.py
+++ main.py
@@ -21,38 +21,38 @@ if __name__ == "__main__":
     df_movilidad = cargar_datos(DATA_PATH)
     
     if df_movilidad is not None:
-       print("Iniciando el proceso de limpieza de datos...")
-       # Limpiar los nombres de las columnas
-       df_limpio = limpiar_nombres_columnas(df_movilidad)
-       df_limpio = convertir_tipos_fechas(df_limpio)
-       df_limpio = eliminar_coordenadas_invalidas(df_limpio)
-       df_limpio = filtar_rango_geografico_valido(df_limpio)
-       
-       print("\n---LIMPIEZA DE DATOS TERMINADO---")
-       
+        print("Iniciando el proceso de limpieza de datos...")
+        # Limpiar los nombres de las columnas
+        df_limpio = limpiar_nombres_columnas(df_movilidad)
+        df_limpio = convertir_tipos_fechas(df_limpio)
+        df_limpio = eliminar_coordenadas_invalidas(df_limpio)
+        df_limpio = filtar_rango_geografico_valido(df_limpio)
+        
+        print("\n---LIMPIEZA DE DATOS TERMINADO---")
+        
         # MÓDULO IMPUTACIÓN DE DATOS
-       print("\n---INICIANDO IMPUTACIÓN DE DATOS---")
-       df_procesado = rellenar_valores_pasajeros_en_cero(df_limpio)
-    
-       print("\n---IMPUTACIÓN DE DATOS TERMINADO---")
-       
-       print("\n---INICIANDO AGREGACIÓN NUEVAS COLUMNAS---")
-       df_final = crear_columna_hora_pickup(df_procesado)
-       df_final = crear_columna_dia_semana_pickup(df_procesado)    
-       df_final = crear_columna_fecha_pickup(df_procesado)
-       df_final = crear_columna_viaje_duracion(df_procesado)
-       df_final = crear_columna_hora_pico(df_procesado)
+        print("\n---INICIANDO IMPUTACIÓN DE DATOS---")
+        df_procesado = rellenar_valores_pasajeros_en_cero(df_limpio)
         
-       print("\n---AGREGACIÓN NUEVAS COLUMNAS TERMINADO---")
-       
-       # GUARDAR DATOS
-       guardar_datos_limpios(df_final, OUTPUT_CLEANED_PATH)
+        print("\n---IMPUTACIÓN DE DATOS TERMINADO---")
         
-       # AQUÍ TERMINÓ EL PIPELINE DE LIMPIEZA
-       print("\n---PIPELINE TERMINADO---")
+        print("\n---INICIANDO AGREGACIÓN NUEVAS COLUMNAS---")
+        df_final = crear_columna_hora_pickup(df_procesado)
+        df_final = crear_columna_dia_semana_pickup(df_procesado)    
+        df_final = crear_columna_fecha_pickup(df_procesado)
+        df_final = crear_columna_viaje_duracion(df_procesado)
+        df_final = crear_columna_hora_pico(df_procesado)
+            
+        print("\n---AGREGACIÓN NUEVAS COLUMNAS TERMINADO---")
         
-       print("\n---Información del DataFrame---")
-       df_final.info(show_counts=True)
+        # GUARDAR DATOS
+        guardar_datos_limpios(df_final, OUTPUT_CLEANED_PATH)
+            
+        # AQUÍ TERMINÓ EL PIPELINE DE LIMPIEZA
+        print("\n---PIPELINE TERMINADO---")
+            
+        print("\n---Información del DataFrame---")
+        df_final.info(show_counts=True)
        
     else:
         print("No se pudieron cargar los datos. Terminando el script.")
